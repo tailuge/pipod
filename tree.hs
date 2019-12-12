dac7ee4e02b8d15c87d7f33559401fd11b2ffb3b
tailuge <tailuge@gmail.com>
2019-12-12 08:19:18 +0000

    tree

--- tree.hs
+++ tree.hs
@@ -1,5 +1,5 @@
 
-data Tree a = TreeLeaf a | TreeNode (Tree a) (Tree a) | Id deriving (Show, Eq) 
+data Tree a = TreeLeaf a | TreeNode (Tree a) (Tree a) deriving (Show, Eq) 
 
 data Bush a = BushLeaf a | BushNode [Bush a] deriving (Show, Eq)
 
@@ -10,10 +10,10 @@ bush = BushNode [BushLeaf 'f', BushNode [BushLeaf 'g', BushLeaf 'x', BushLeaf 'y
 
 mcurry :: Bush a -> Tree a
 mcurry (BushLeaf x) = TreeLeaf x
-mcurry (BushNode lst) = foldl (\acc elt -> TreeNode acc (mcurry elt)) Id lst
+mcurry (BushNode (x:xs)) = foldl (\acc elt -> TreeNode acc (mcurry elt)) (mcurry x) xs
 
 -- flatten ?
 
 flatten :: Bush a -> [a]
 flatten (BushLeaf x) = [x]
-flatten (BushNode lst) = foldl (\acc elt -> acc ++ (flatten elt)) [] lst
+flatten (BushNode (x:lst)) = foldl (\acc elt -> acc ++ (flatten elt)) (flatten x) lst
